acdc30d30ca5a3c5eb3b371a8fb5242b3587691e
Raiii02 <raihandzikra01@gmail.com>
2024-07-25 13:36:34 +0700

    add send email

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OurTeamController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\PublicationController;
+use App\Jobs\SendEmailJob;
+use App\Mail\SendEmail;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -39,3 +43,13 @@ Route::get('/success', [DonateController::class, 'success'])->name('success');
 Route::get('team', [OurTeamController::class, 'index'])->name('team');
 Route::get('contact', [ContacController::class, 'index'])->name('contact');
 Route::get('service', [ServiceController::class, 'index'])->name('service');
+
+
+Route::post('send-email', function (Request $request) {
+    
+    $data['email'] = 'raihandzikra20@gmail.com';
+
+    dispatch(new SendEmailJob($data));
+
+    return 'success';
+});
