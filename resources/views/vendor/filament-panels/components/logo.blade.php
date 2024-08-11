eaa27880c79cd0ece6be4e4d8ec2369aa1cae095
Raiii02 <raihandzikra01@gmail.com>
2024-08-11 15:13:07 +0700

    Icon AYD

---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-2">
+    <img src="{{ asset('assets/img/logo.png') }}" alt="logo" class="h-11">
+    <span class="text-lg font-bold">Anshor Yatim Dhuafa</span>
+</div>
